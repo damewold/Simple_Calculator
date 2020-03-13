dee56aa77d8eb71a1e651e6b13feb6aee624841a
Denis Woldemariam <damewold@gmail.com>
2020-03-13 16:18:38 -0500

    inserted server.js, client.js, jquery.js and style.css files

---
+++ server/public/client.js
@@ -0,0 +1,10 @@
+//check if the client.js sourced properly
+console.log('Hello');
+//check if the jquery file sourced properly
+$(document).ready(onReady);
+
+//write function onReady
+
+function onReady(){
+    console.log('in onReady');
+};
