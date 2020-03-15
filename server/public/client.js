26cdceb3f269e46ce40f6938c0634d1e8a2a1548
Denis Woldemariam <damewold@gmail.com>
2020-03-15 13:11:17 -0500

    finished writing appendHistroyToDom

--- server/public/client.js
+++ server/public/client.js
@@ -7,17 +7,25 @@ $(document).ready(onReady);
 
 function onReady() {
     console.log('in onReady');
-    // $('#add-btn').on('click', operationCalculate);
+    $('.key-operator').on('click', postInputToServer);
     // $('#subtract-btn').on('click', operationCalculate);
     // $('#Multiply-btn').on('click', operationCalculate);
     // $('#divide-btn').on('click', operationCalculate);
-    $('#submit-btn').on('click', postInputToServer);
+    // $('#submit-btn').on('click', postInputToServer);
     // $('#clear-btn').on('click', clearHistory);
 
 };
 
 
 //write postInputToServer function
+function changeKeyToVar (event){
+    event.preventDefault();
+    $('.key-operator').removeClass('selected');
+     operator=this.id;
+    $(this).addClass('selected')
+    console.log('in changekeyToVar',operator);
+    postInputToServer();
+};
 
 function postInputToServer(event) {
     event.preventDefault();
@@ -28,12 +36,13 @@ function postInputToServer(event) {
     $('#number1-in').val('');
     $('#number2-in').val('');
 
-    if (validateInputs(number1, number2)) {
+    // if (validateInputs(number1,operator, number2)) {
 
-        let inputNumbers = { number1, number2 };
+        let inputNumbers = { number1,operator, number2 };
+        
 
         $.ajax({
-            method: 'POST',
+            method:'POST',
             url: '/inputs',
             data: inputNumbers
         })
@@ -44,19 +53,38 @@ function postInputToServer(event) {
                 console.log('Error:', error);
             })
 
-        operationCalculate();
+        // operationCalculate();
     };
 
-};
+// };
 
-function validateInputs( number1, number2 ) {
-    event.preventDefault();
-    console.log( 'Validating inputs' );
-    if ( !number1 || !number2 ) {
-     alert( 'Two inputs are required!' );
-      return false;
-    }else {
-      return true;
-    }
-  };
+// function validateInputs( number1,operator, number2 ) {
+//     event.preventDefault();
+//     console.log( 'Validating inputs',operator,number1,number2 );
+//     if ( !number1 || !number2 ) {
+//      alert( 'Two inputs are required!' );
+//       return false;
+//     }else {
+//       return true;
+//     }
+//   };
+
+//   function operationCalculate(event){
+//       event.preventDefault();
+//       console.log('in operationCalculate');
+      
+//  $.ajax({
+//      method:'GET',
+//      url:'/operations'
+//  })
+
+//  .then( function(response){
+//      console.log('Got response inoperationCalculate', response);
+    
+//  })
+//  .catch(function(error){
+//      console.log('Error',error);
+//  })
+
+//   };
 
--- server/server.js
+++ server/server.js
@@ -10,13 +10,34 @@ app.use(express.static('server/public'));
 
 //let the client know that you have received the input Numbers
 
+let calcResult;
+let numberArray=[];
+
 app.post('/inputs', (req,res) =>{
   let numberObj=(req.body);
   number1=numberObj.number1;
   number2=numberObj.number2;
+  operator=numberObj.operator;
   console.log('Got input from client',numberObj);
+  if (numberObj.operator === 'add-btn') {
+    result = Number(number1) + Number(number2);
+  } else if (numberObj.operator  === 'subtract-btn') {
+    result = Number(number1) - Number(number2)
+  } else if (numberObj.operator  === 'multiply-btn') {
+    result = Number(number1) * Number(number2)
+  } else if (numberObj.operator  === 'divide-btn') {
+    result =  Number(number1) / Number(number2)
+  };
+   calcResult={number1:number1,
+    number2:number2,
+    result:result
+   };
+  console.log(calcResult);
+
   res.sendStatus(200);
+  numberArray.push(calcResult)
 })
+
 
 
 
